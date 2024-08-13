31611b4f751c22baaf478d7bb5fa6057f39da1b2
sner21 <414975415@qq.com>
2024-08-13 19:25:11 +0800

    20240813

--- src/utils/controller.rs
+++ src/utils/controller.rs
@@ -1,19 +1,17 @@
 use windows::Win32::Foundation::HWND;
-use windows::Win32::UI::WindowsAndMessaging::{GetWindowLongA, GWL_STYLE, SetWindowLongA, SHOW_WINDOW_CMD, ShowWindow};
+use windows::Win32::UI::WindowsAndMessaging::{GetWindowLongA, GWL_STYLE,SetWindowLongA, SHOW_WINDOW_CMD, ShowWindow,SetWindowPos,HWND_TOPMOST,SWP_NOMOVE, SWP_NOSIZE};
 
 
 pub fn frameless(hwnd: HWND) {
     unsafe {
         let style = GetWindowLongA(hwnd, GWL_STYLE);
-        println!("{}", style);
-        SetWindowLongA(hwnd, GWL_STYLE, style - 0x00C00000);
+        ShowWindow(hwnd, SHOW_WINDOW_CMD(3));
+        SetWindowLongA(hwnd, GWL_STYLE, style - 12582912u32 as i32 );
         ShowWindow(hwnd, SHOW_WINDOW_CMD(3));
     }
 }
 pub fn overhead(hwnd: HWND) {
     unsafe {
-        let style = GetWindowLongA(hwnd, GWL_STYLE);
-        println!("{}", style);
-        SetWindowLongA(hwnd, GWL_STYLE, style | 128u32 as i32 | 262144u32 as i32);
+        SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
     }
 }
